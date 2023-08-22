2693e8730a0394d4695e8f80438cbf74b5fbcbd3
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-08-22 12:47:31 -0600

    Uploading a file, posting the data to the database and getting it back works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  // State to hold the parsed data from CSV
   const [parsedData, setParsedData] = useState<any[]>([]);
+  const [songData, setSongData] = useState<any[]>([]); // New state for fetched song data
 
-  // Function to handle file upload
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
@@ -13,7 +12,6 @@ function App() {
     }
   };
 
-  // Function to process the uploaded CSV file
   const processCSV = (file: File) => {
     const reader = new FileReader();
 
@@ -22,7 +20,8 @@ function App() {
       const lines = text.split('\n');
 
       // Parse each line into an object
-      const parsedData = lines.map((line) => {
+      const parsedData = lines.map((line, index) => {
+        if (index === 0) return null; // Skip header line
         const [song_name, band, year] = line
           .split(';')
           .map((value) => value.trim());
@@ -30,17 +29,51 @@ function App() {
         return { song_name, band, year };
       });
 
-      parsedData.shift(); // Remove header line
-
-      setParsedData(parsedData); // Update state with parsed data
+      setParsedData(parsedData.filter(Boolean)); // Filter out null values
     };
 
     reader.readAsText(file);
   };
 
+  const postSongsToDatabase = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/songs/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(parsedData),
+      });
+
+      if (response.ok) {
+        // If data was successfully posted, fetch the updated song data from the database
+        fetchSongData();
+      } else {
+        console.error('Error posting songs:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error posting songs:', error);
+    }
+  };
+
+  const fetchSongData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/songs/'); // Adjust the endpoint URL
+      const data = await response.json();
+      setSongData(data);
+    } catch (error) {
+      console.error('Error fetching song data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSongData();
+  }, []); // Fetch song data when the component mounts
+
   return (
     <div>
       <input type="file" accept=".csv" onChange={handleFileUpload} />
+      <button onClick={postSongsToDatabase}>Post Songs to Database</button>
       <table>
         <thead>
           <tr>
@@ -50,7 +83,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {parsedData.map((song, index) => (
+          {songData.map((song, index) => (
             <tr key={index}>
               <td>{song.song_name}</td>
               <td>{song.band}</td>
@@ -64,3 +97,85 @@ function App() {
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+// import './App.css';
+
+// function App() {
+//   const [parsedData, setParsedData] = useState<any[]>([]);
+
+//   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const file = event.target.files?.[0];
+//     if (file) {
+//       processCSV(file);
+//     }
+//   };
+
+//   const processCSV = (file: File) => {
+//     const reader = new FileReader();
+
+//     reader.onload = (e: ProgressEvent<FileReader>) => {
+//       const text = e.target?.result as string;
+//       const lines = text.split('\n');
+
+//       // Parse each line into an object
+//       const parsedData = lines.map((line, index) => {
+//         if (index === 0) return null; // Skip header line
+//         const [song_name, band, year] = line
+//           .split(';')
+//           .map((value) => value.trim());
+
+//         return { song_name, band, year };
+//       });
+
+//       setParsedData(parsedData.filter(Boolean)); // Filter out null values
+//     };
+
+//     reader.readAsText(file);
+//   };
+
+//   const postSongsToDatabase = async () => {
+//     // Assuming you have an API call here to post parsedData to the endpoint song/add
+//     // Make sure to handle errors and provide appropriate feedback to the user
+//     try {
+//       const response = await fetch('http://localhost:3000/songs/add', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(parsedData),
+//       });
+//       // Handle response, update UI accordingly
+//     } catch (error) {
+//       console.error('Error posting songs:', error);
+//       // Handle error, show error message to user
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <input type="file" accept=".csv" onChange={handleFileUpload} />
+//       <button onClick={postSongsToDatabase}>Post Songs to Database</button>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Song Name</th>
+//             <th>Band</th>
+//             <th>Year</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {parsedData.map((song, index) => (
+//             <tr key={index}>
+//               <td>{song.song_name}</td>
+//               <td>{song.band}</td>
+//               <td>{song.year}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
+
+// export default App;
